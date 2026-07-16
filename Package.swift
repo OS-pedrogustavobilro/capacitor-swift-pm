a04688a48ba67ef65f4137e171d516798d9f7f1d
OS-pedrogustavobilro <OS-pedrogustavobilro@users.noreply.github.com>
2026-07-16 16:09:35 +0000

    Commit from GitHub Actions (Dispatch Release)

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/OS-pedrogustavobilro/capacitor-swift-pm/releases/download/7.6.8-test.1/Capacitor.xcframework.zip",
-            checksum: "0e0c834784186b14fb71e1cce566456aa309f2fb19f3a98c438249ac5f37f024"
+            url: "https://github.com/OS-pedrogustavobilro/capacitor-swift-pm/releases/download/8.4.2-test.1/Capacitor.xcframework.zip",
+            checksum: "55eb9e4a872d01fed8448446f3ac0d7a922d74439608e73533402c510cb975e5"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/OS-pedrogustavobilro/capacitor-swift-pm/releases/download/7.6.8-test.1/Cordova.xcframework.zip",
-            checksum: "93b96732c1d2b2d08626333e4239c112a1dc82a62b8684cd433acb7afd44132c"
+            url: "https://github.com/OS-pedrogustavobilro/capacitor-swift-pm/releases/download/8.4.2-test.1/Cordova.xcframework.zip",
+            checksum: "4c6bb978a2e86b98e320e01be38e4b9a8b4180e791792d925d41f9f26ca71707"
         )
     ]
 )
